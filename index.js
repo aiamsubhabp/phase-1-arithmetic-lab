19feb053f9099580cae734c7e823c1afea4bcb0e
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-02-25 20:24:54 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let multiply;
+let num1 = 2;
+let num2 = 31;
+multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*10)+1;
+
+let num3=34;
+let num4=15;
+let mod = num3 % num4;
+
+let max = Math.max(1,2,20);
+
